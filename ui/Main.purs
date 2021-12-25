7b6ca599a22f7e60a936fa579491f29f34e7f223
Stefan Matting <stefan.matting@gmail.com>
2021-12-25 01:31:06 +0100

    basic rendering

--- ui/Main.purs
+++ ui/Main.purs
@@ -7,6 +7,7 @@ import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
+import Web.HTML.HTMLElement as HTMLElement
 import Halogen.Subscription as HS
 import Effect (Effect)
 import Effect.Class (class MonadEffect, liftEffect)
@@ -15,10 +16,26 @@ import Halogen.VDom.Driver (runUI)
 import Callback (registerCallback)
 import Effect.Console as Console
 import Types (Tab)
+import Web.HTML.Common (ClassName(..))
+import Web.UIEvent.InputEvent as InputEvent
+import Data.Array as A
+import Halogen.Query.Input (RefLabel(..))
+import Data.Foldable
 
-type State = { enabled :: Boolean }
+type State = { enabled :: Boolean,
+               tabs :: Array Tab,
+               sortedTabs :: Array Tab,
+               searchQuery :: String,
+               selectedIndex :: Int
+             }
 
-data Action = Toggle | Initialize | Tabs (Array Tab)
+data Action = Toggle
+            | Initialize
+            | Tabs (Array Tab)
+            | HighlightTab Tab
+            | CloseTab Tab
+            | UpdateSearch String
+            | NoOp
 
 component :: forall q i o m. MonadEffect m => H.Component q i o m
 component =
@@ -29,20 +46,61 @@ component =
     }
 
 initialState :: forall i. i -> State
-initialState _ = { enabled: false }
+initialState _ =
+  { enabled: false,
+    tabs: [],
+    sortedTabs: [],
+    searchQuery: "",
+    selectedIndex: 0
+  }
 
 render :: forall m. State -> H.ComponentHTML Action () m
 render state =
-  let
-    label = if state.enabled then "On!!!" else "Off"
-  in
-    HH.button
-      [ HP.title label
-      , HE.onClick \_ -> Toggle
-      ]
-      [ HH.text label ]
+  HH.div
+    [ HP.id "tab-view",
+      HP.tabIndex 0
+      -- TODO: keydown
+    ]
+    [ HH.input
+        [ HP.id "tab-search",
+          HP.type_ HP.InputText,
+          HE.onValueInput UpdateSearch,
+          HP.placeholder "Type to search...",
+          HP.ref (RefLabel "tabSearch")
+        ],
+      HH.ol
+        []
+        (A.mapWithIndex (renderTab state.selectedIndex) state.sortedTabs)
+    ]
 
-handleAction ∷ forall o m. MonadEffect m => Action → H.HalogenM State Action () o m Unit
+renderTab :: forall m. Int -> Int -> Tab -> H.ComponentHTML Action () m
+renderTab selectedIndex index tab =
+  let
+    iconBg = "background-image: url('" <> tab.favIconUrl <> "')"
+    selected = selectedIndex == index
+    cls name enable = if enable then[ ClassName name ] else []
+  in
+   HH.li
+    [ HP.classes (cls "tab" true <> cls "selected" selected),
+      HE.onClick \_ -> HighlightTab tab
+    ]
+    [ HH.span [ HP.class_ (ClassName "tab-close-button"), HE.onClick \_ -> CloseTab tab]
+              [ HH.span [] [HH.text "close"]],
+      HH.span [ HP.class_ (ClassName "tab-icon-wrap") ]
+              [ HH.span
+                  [ HP.class_ (ClassName "tab-icon"),
+                    HP.style iconBg
+                  ]
+                  []
+              ],
+      HH.span [ HP.class_ (ClassName "tab-title"),
+                HP.class_ (ClassName "tab-link")
+              ]
+              [ HH.span [] [ HH.text tab.title ] ]
+    ]
+
+
+handleAction :: forall o m. MonadEffect m => Action → H.HalogenM State Action () o m Unit
 handleAction = case _ of
   Initialize -> do
     { emitter, listener } <- H.liftEffect HS.create
@@ -55,8 +113,27 @@ handleAction = case _ of
   Toggle ->
     H.modify_ \st -> st { enabled = not st.enabled }
 
-  Tabs tabs ->
-    H.liftEffect $ Console.log ("tabs " <> show tabs)
+  Tabs tabs -> do
+    -- H.liftEffect $ Console.log ("tabs " <> show tabs)
+    H.modify_ \st -> st { tabs = tabs, sortedTabs = tabs }
+    mbEl <- H.getHTMLElementRef (RefLabel "tabSearch")
+    for_ mbEl \el ->
+      liftEffect $ HTMLElement.focus el
+    pure unit
+
+  HighlightTab tab ->
+    pure unit
+
+  CloseTab tab ->
+    pure unit
+
+  UpdateSearch query -> do
+    -- H.liftEffect $ Console.log query
+    pure unit
+
+  NoOp -> do
+    -- H.liftEffect $ Console.log "no-op"
+    pure unit
 
 
 main :: Effect Unit
