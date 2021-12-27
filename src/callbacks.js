2f4377201d23657a6e5fc949f2eb34728aecf187
Stefan Matting <stefan.matting@gmail.com>
2021-12-28 00:36:04 +0100

    remaining

---
+++ src/callbacks.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var callbacks = (function() {
+
+    function register(name, fn) {
+        // console.log('this is register', name);
+        // console.log('this is fn', fn);
+        window.cbks = window.cbks || {};
+        window.cbks[name] = window.cbks[name] || [];
+        window.cbks[name].push(fn);
+    }
+
+    function call(name, arg) {
+        window.cbks = window.cbks || {};
+        window.cbks[name] = window.cbks[name] || [];
+        window.cbks[name].forEach(function(callback) {
+            // console.log(window.cbks);
+            // console.log('callback is', callback);
+            // console.log('arg is', arg);
+            return callback(arg);
+        });
+    }
+
+    return ({
+        register: register,
+        call: call
+    });
+
+})();
